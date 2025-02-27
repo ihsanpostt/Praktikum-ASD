2c450a1993810544997f529885784d12c0e87185
ihsanpostt <burhanuddin913@sma.belajar.id>
2025-02-27 17:03:22 +0700

    pertanyaan percobaan 2

---
+++ Jobsheet3/Mahasiswa6.java
@@ -0,0 +1,12 @@
+public class Mahasiswa6 {
+    public String nim,nama,kelas;
+    public float ipk;
+
+    void cetakInformasi(){
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("----------------------------------");
+    }
+}
