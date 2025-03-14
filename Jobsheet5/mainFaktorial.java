fa8aff1a36a2031893988c0008d47e11241032d4
ihsanpostt <burhanuddin913@sma.belajar.id>
2025-03-14 10:15:13 +0700

    Percobaan 1

---
+++ Jobsheet5/mainFaktorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class mainFaktorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nilai faktorial: ");
+        int nilai = scanner.nextInt();
+
+        Faktorial fk = new Faktorial();
+
+        System.out.println("Nilai faktorial " + nilai + " menggunakan BF: " + fk.faktorialBF(nilai));
+        System.out.println("Nilai faktorial " + nilai + " menggunakan DC: " + fk.faktorialDC(nilai));
+    }
+}
