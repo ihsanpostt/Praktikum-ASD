d9f64f76d61a0b0e06a1a6599b0bd339f97a500d
ihsanpostt <burhanuddin913@sma.belajar.id>
2025-03-21 03:06:42 +0700

    Selection sort

--- joobshet6/MahasiswaBerprestasi06.java
+++ joobshet6/MahasiswaBerprestasi06.java
@@ -22,7 +22,7 @@ public class MahasiswaBerprestasi06 {
     void bubbleSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
             for (int j = 1; j < listMhs.length - i; j++) {
-                if (listMhs[j] != null && listMhs[j - 1] != null) { // Cek null untuk menghindari error
+                if (listMhs[j] != null && listMhs[j - 1] != null) { 
                     if (listMhs[j].ipk > listMhs[j - 1].ipk) {
                         Mahasiswa06 tmp = listMhs[j];
                         listMhs[j] = listMhs[j - 1];
@@ -30,6 +30,22 @@ public class MahasiswaBerprestasi06 {
                     }
                 }
             }
+        }
+    }
+
+    void selectionSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            int idxMin = i;
+            for (int j = i + 1; j < listMhs.length; j++) {
+                if (listMhs[j] != null && listMhs[idxMin] != null) { 
+                    if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                        idxMin = j;
+                    }
+                }
+            }
+            Mahasiswa06 tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
         }
     }
 }
--- joobshet6/MahasiswaDemo06.java
+++ joobshet6/MahasiswaDemo06.java
@@ -10,28 +10,29 @@ public class MahasiswaDemo06 {
 
         for (int i = 0; i < 5; i++) {
             System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
-
-            System.out.print("NIM   : ");
+            System.out.print("NIM    : ");
             String nim = sc.nextLine();
-
-            System.out.print("Nama  : ");
+            System.out.print("Nama   : ");
             String nama = sc.nextLine();
-
-            System.out.print("Kelas : ");
+            System.out.print("Kelas  : ");
             String kelas = sc.nextLine();
-
-            System.out.print("IPK   : ");
-            String ipkStr = sc.nextLine().replace(",", "."); 
+            System.out.print("IPK    : ");
+            String ipkStr = sc.nextLine().replace(",", ".");
             double ipk = Double.parseDouble(ipkStr);
 
             Mahasiswa06 m = new Mahasiswa06(nim, nama, kelas, ipk);
             list.tambah(m);
-
             System.out.println("-----------------------------");
         }
 
-        System.out.println("\nData Mahasiswa Setelah Sorting Berdasarkan IPK (DESC):");
+        System.out.println();
+        System.out.println("Data Mahasiswa Setelah Sorting Berdasarkan IPK (DESC):");
         list.bubbleSort();
+        list.tampil();
+
+        System.out.println();
+        System.out.println("Data Mahasiswa Setelah Sorting Menggunakan SELECTION SORT (ASC):");
+        list.selectionSort();
         list.tampil();
     }
 }
