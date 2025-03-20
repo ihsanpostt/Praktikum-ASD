5d47fbeb65581a030790c3af05fe957ac163382a
ihsanpostt <burhanuddin913@sma.belajar.id>
2025-03-20 22:57:17 +0700

    pertanyaan bubblesort

--- joobshet6/MahasiswaBerprestasi06.java
+++ joobshet6/MahasiswaBerprestasi06.java
@@ -7,25 +7,27 @@ public class MahasiswaBerprestasi06 {
             listMhs[idx] = m;
             idx++;
         } else {
-            System.out.println("data sudah penuh");
+            System.out.println("Data sudah penuh");
         }
     }
 
     void tampil() {
         for (Mahasiswa06 m : listMhs) {
-            m.tampilkanInformasi
-            ();
-            System.out.println("-----------------------------");
+            if (m != null) {
+                m.tampilkanInformasi();
+            }
         }
     }
 
     void bubbleSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
             for (int j = 1; j < listMhs.length - i; j++) {
-                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
-                    Mahasiswa06 tmp = listMhs[j];
-                    listMhs[j] = listMhs[j - 1];
-                    listMhs[j - 1] = tmp;
+                if (listMhs[j] != null && listMhs[j - 1] != null) { // Cek null untuk menghindari error
+                    if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                        Mahasiswa06 tmp = listMhs[j];
+                        listMhs[j] = listMhs[j - 1];
+                        listMhs[j - 1] = tmp;
+                    }
                 }
             }
         }
--- joobshet6/MahasiswaDemo06.java
+++ joobshet6/MahasiswaDemo06.java
@@ -1,20 +1,37 @@
+import java.util.Scanner;
+
 public class MahasiswaDemo06 {
     public static void main(String[] args) {
-    MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06();
-    Mahasiswa06 m1 = new Mahasiswa06("123", "Zidan", "2A", 3.2);
-    Mahasiswa06 m2 = new Mahasiswa06("124", "Ayu", "2A", 3.5);
-    Mahasiswa06 m3 = new Mahasiswa06("125", "Sofi", "2A", 3.1);
-    Mahasiswa06 m4 = new Mahasiswa06("126", "Sita", "2A", 3.9);
-    Mahasiswa06 m5 = new Mahasiswa06("127", "Miki", "2A", 3.7);
-    list.tambah(m1);
-    list.tambah(m2);
-    list.tambah(m3);
-    list.tambah(m4);
-    list.tambah(m5);
-    System.out.println("Data Mahasiswa Sebelum Sorting : ");
-    list.tampil();
-    System.out.println("Data Mahasiswa Sesudah Sorting Berdasarkan IPK (DESC) : ");
-    list.bubbleSort();
-    list.tampil();
+        Scanner sc = new Scanner(System.in);
+        MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06();
+
+        System.out.println("Data Mahasiswa Sebelum Sorting:");
+        list.tampil();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK   : ");
+            String ipkStr = sc.nextLine().replace(",", "."); 
+            double ipk = Double.parseDouble(ipkStr);
+
+            Mahasiswa06 m = new Mahasiswa06(nim, nama, kelas, ipk);
+            list.tambah(m);
+
+            System.out.println("-----------------------------");
+        }
+
+        System.out.println("\nData Mahasiswa Setelah Sorting Berdasarkan IPK (DESC):");
+        list.bubbleSort();
+        list.tampil();
     }
 }
