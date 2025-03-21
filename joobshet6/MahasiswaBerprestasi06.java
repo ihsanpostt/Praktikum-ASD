06c1daf5579e9e4d7cf55e1857eefe1c8cef2cba
ihsanpostt <burhanuddin913@sma.belajar.id>
2025-03-21 14:44:00 +0700

    Insertion Sort

--- joobshet6/MahasiswaBerprestasi06.java
+++ joobshet6/MahasiswaBerprestasi06.java
@@ -22,12 +22,10 @@ public class MahasiswaBerprestasi06 {
     void bubbleSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
             for (int j = 1; j < listMhs.length - i; j++) {
-                if (listMhs[j] != null && listMhs[j - 1] != null) { 
-                    if (listMhs[j].ipk > listMhs[j - 1].ipk) {
-                        Mahasiswa06 tmp = listMhs[j];
-                        listMhs[j] = listMhs[j - 1];
-                        listMhs[j - 1] = tmp;
-                    }
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa06 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
                 }
             }
         }
@@ -37,15 +35,25 @@ public class MahasiswaBerprestasi06 {
         for (int i = 0; i < listMhs.length - 1; i++) {
             int idxMin = i;
             for (int j = i + 1; j < listMhs.length; j++) {
-                if (listMhs[j] != null && listMhs[idxMin] != null) { 
-                    if (listMhs[j].ipk < listMhs[idxMin].ipk) {
-                        idxMin = j;
-                    }
+                if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                    idxMin = j;
                 }
             }
             Mahasiswa06 tmp = listMhs[idxMin];
             listMhs[idxMin] = listMhs[i];
             listMhs[i] = tmp;
+        }
+    }
+
+    void insertionSort() {
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa06 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
         }
     }
 }
--- joobshet6/MahasiswaDemo06.java
+++ joobshet6/MahasiswaDemo06.java
@@ -4,35 +4,41 @@ public class MahasiswaDemo06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06();
-
-        System.out.println("Data Mahasiswa Sebelum Sorting:");
+        
+        System.out.println("Data Mahasiswa Sebelum Sorting : ");
         list.tampil();
-
+        
         for (int i = 0; i < 5; i++) {
             System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
-            System.out.print("NIM    : ");
+            System.out.print("NIM : ");
             String nim = sc.nextLine();
-            System.out.print("Nama   : ");
+            System.out.print("Nama : ");
             String nama = sc.nextLine();
-            System.out.print("Kelas  : ");
+            System.out.print("Kelas : ");
             String kelas = sc.nextLine();
-            System.out.print("IPK    : ");
+            System.out.print("IPK : ");
             String ipkStr = sc.nextLine().replace(",", ".");
             double ipk = Double.parseDouble(ipkStr);
-
+            
             Mahasiswa06 m = new Mahasiswa06(nim, nama, kelas, ipk);
             list.tambah(m);
+            
             System.out.println("-----------------------------");
         }
-
+        
         System.out.println();
-        System.out.println("Data Mahasiswa Setelah Sorting Berdasarkan IPK (DESC):");
+        System.out.println("Data Mahasiswa Sesudah Sorting Berdasarkan IPK (DESC) : ");
         list.bubbleSort();
         list.tampil();
-
+        
         System.out.println();
-        System.out.println("Data Mahasiswa Setelah Sorting Menggunakan SELECTION SORT (ASC):");
+        System.out.println("Data Yang Sudah Terurut Menggunakan SELECTION SORT (ASC)");
         list.selectionSort();
+        list.tampil();
+        
+        System.out.println();
+        System.out.println("Data Yang Sudah Terurut Menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
         list.tampil();
     }
 }
