422f0194975da4c76499b34793fbaff2de234e77
ihsanpostt <burhanuddin913@sma.belajar.id>
2025-05-16 20:20:28 +0700

    percobaan

---
+++ jobsheet10/Queue.java
@@ -0,0 +1,91 @@
+public class Queue {
+    int[] data;
+    int front, rear, size, max;
+
+    Queue (int n) {
+        max = n;
+        data = new int[n];
+        front = -1;
+        size = 0;
+        rear = -1;
+    }
+
+    boolean isEmpty() {
+        return size == 0;
+    }
+
+    boolean isFull() {
+        return size == max;
+    }
+
+    void peek() {
+        if (!isEmpty()) {
+            System.out.println("Elemen terdepan : " + data[front]);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    void print() {
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            int i = front;
+            while (i != rear) {
+                System.out.println(data[i] + " ");
+                i = (i+1) % max;
+            }
+            System.out.println(data[i] + " ");
+            System.out.println("Jumlah elemen = " + size);
+        }
+    }
+
+    void clear() {
+        if (!isEmpty()) {
+            front = -1;
+            rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    void enqueue (int num) {
+        if (isFull()) {
+            return;
+        } else {
+            if (isEmpty()) {
+                front = rear = 0;
+            } else {
+                if (rear == max - 1) {
+                    rear = 0;
+                } else {
+                    rear++;
+                }
+            }
+        }
+        data[rear] = num;
+        size++;
+    }
+
+    int dequeue () {
+        int num = 0;
+        if (isEmpty()) {
+            System.out.println("queue masih kosong");
+        } else {
+            num = data[front];
+            size--;
+            if (isEmpty()) {
+                front = rear = -1;
+            } else {
+                if (front == max -1) {
+                    front = 0;
+                } else {
+                    front++;
+                }
+            }
+        }
+        return num;
+    }
+}
